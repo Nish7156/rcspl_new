5f630a7a4f3e24f7b4da9cc2c1d481e71543b363
Nishant Tanaji Shedage <nishantt@geekyants.com>
2023-09-03 22:24:14 +0530

    header

---
+++ src/js/headerFunctions.js
@@ -0,0 +1,37 @@
+export function setupHeaderFunctions() {
+    const navLinks = document.querySelector(".nav-links");
+    const menuOpenBtn = document.querySelector(".navbar .bx-menu");
+    const menuCloseBtn = document.querySelector(".nav-links .bx-x");
+    const htmlcssArrow = document.querySelector(".htmlcss-arrow");
+    const moreArrow = document.querySelector(".more-arrow");
+    const jsArrow = document.querySelector(".js-arrow");
+  
+    menuOpenBtn.addEventListener("click", () => {
+      navLinks.style.left = "0";
+    });
+  
+    menuCloseBtn.addEventListener("click", () => {
+      navLinks.style.left = "-100%";
+    });
+  
+    htmlcssArrow.addEventListener("click", () => {
+      navLinks.classList.toggle("show1");
+    });
+  
+    moreArrow.addEventListener("click", () => {
+      navLinks.classList.toggle("show2");
+    });
+  
+    jsArrow.addEventListener("click", () => {
+      navLinks.classList.toggle("show3");
+    });
+  
+    return () => {
+      menuOpenBtn.removeEventListener("click");
+      menuCloseBtn.removeEventListener("click");
+      htmlcssArrow.removeEventListener("click");
+      moreArrow.removeEventListener("click");
+      jsArrow.removeEventListener("click");
+    };
+  }
+  
